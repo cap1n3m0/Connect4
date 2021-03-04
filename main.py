9f3bb4dc4d16379a6b982fed3fb3716e439081b0
Vedant Jhawar <34079597+cap1n3m0@users.noreply.github.com>
2021-03-04 17:36:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ def printboard(board):
             print(l, end="")
         print("")
 
+
+def checkWin(): 
+    pass
+
+
 printboard(board)
 
 xPos = 0
@@ -19,13 +24,16 @@ yPos  = 0
 win = False
 
 while not win: 
-    print("Choose a position: ")
+    print("Player red, choose a position: ")
     xPos = int(input())
     yPos = int(input())
     board[xPos][yPos] = "r"
-    print("Choose a position: ")
+    if checkWin("r"):
+        win = True
+    print("Player yellow, choose a position: ")
     xPos = int(input())
     yPos = int(input())
     board[xPos][yPos] = "y"
-
+    if checkWin("y"):
+        win = True
 
