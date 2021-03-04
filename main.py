191d2f058919873726a1abc60ab5bf2a30a9c924
coolumair <41354571+coolumair@users.noreply.github.com>
2021-03-04 17:26:30 -0500

    Printing the board
    the board is created and there is a function to print it

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 print("this is a tset")
 print("testing recieved")
+
+board = [
+["o", "o", "o", "o", "o", "o", "o"],
+["o", "o", "o", "o", "o", "o", "o"],
+["o", "o", "o", "o", "o", "o", "o"],
+["o", "o", "o", "o", "o", "o", "o"],
+["o", "o", "o", "o", "o", "o", "o"],
+["o", "o", "o", "o", "o", "o", "o"],]
+
+def printboard(board):
+    for i in board:
+        print("| ", end="")
+        for l in i:
+            print(l, end="")
+            print(" | ", end="")
+        print("")
+        print("-----------------------------")
