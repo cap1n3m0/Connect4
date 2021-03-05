7949e59bf0056dbf1a774b40fc0d7f538a9d8fcf
CommanderKV <kylervisser@gmail.com>
2021-03-05 17:19:20 -0500

    modified:   main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from Player import Player
-import Ai
+from AiStuff import MainAi
 from one import *
 
 choice = input("Do you want to play AI or 1 v 1? Enter AI or 1: ")
 if choice == "AI":
-    pass
+    MainAi.runAI()
 elif choice == "one":
     runOne()
